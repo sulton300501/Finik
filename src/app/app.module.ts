cf3db612aa854ca361ee8edcf277a6018e82b9ae
sulton <sultonmalik326@gmail.com>
2024-05-02 21:26:33 +0500

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DostavkaComponent } from './dostavka/dostavka.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { GarantiyaComponent } from './garantiya/garantiya.component';
+import { CompanyComponent } from './company/company.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DostavkaComponent,
+    NavbarComponent,
+    FooterComponent,
+    GarantiyaComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  
+    
+   
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/garantiya/garantiya.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-garantiya',
+  templateUrl: './garantiya.component.html',
+  styleUrl: './garantiya.component.css'
+})
+export class GarantiyaComponent {
+
+}
